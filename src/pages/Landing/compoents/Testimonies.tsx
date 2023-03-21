7eaf30a52aa12fd0e73c3cf8fe09d8184d598b4c
Bolade Akinniyi <boladebode@gmail.com>
2023-03-21 07:12:24 +0100

    fix: testimony images

--- src/pages/Landing/compoents/Testimonies.tsx
+++ src/pages/Landing/compoents/Testimonies.tsx
@@ -2,26 +2,36 @@ import { Box, Flex, Text, Image, Heading } from "@chakra-ui/react";
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
+import mercy from "assets/testimony/mercy.jpeg";
+import undisclosed1 from "assets/testimony/undisclosed1.jpeg";
+import olajide from "assets/testimony/olajide.jpeg";
+import chinedu from "assets/testimony/chinedu.jpeg";
 
 // sample testimonial data
 const testimonials = [
   {
     id: 1,
-    name: "John Doe",
-    image: "/path/to/image1.jpg",
-    text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin aliquam velit eu ultrices iaculis. Praesent congue erat vel nisl suscipit, ut suscipit dui iaculis.",
+    name: "Mercy",
+    image: mercy,
+    text: "I found my soulmate through this platform after seeing their Facebook ad. I was skeptical at first but decided to give it a try. To my surprise, it worked! We got married on October 31, 2020. Thank you for helping me find the love of my life!",
   },
   {
     id: 2,
-    name: "Jane Smith",
-    image: "/path/to/image2.jpg",
-    text: "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
+    name: "Undisclosed",
+    image: undisclosed1,
+    text: "I found my wife in July 2021 after leaving the platform for someone else in 2020. But in just 2 weeks of coming back, I got connected to my wife. Married on October 30, 2021, and our marriage is a huge success. Forever grateful to this platform!",
   },
   {
     id: 3,
-    name: "Bob Johnson",
-    image: "/path/to/image3.jpg",
-    text: "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.",
+    name: "Mr Olajide",
+    image: olajide,
+    text: "'At 64yrs old, I contacted Mrs Fatima for a match to find a new wife after losing mine. I requested a young beautiful lady who can have a child for me. Funke, 34yrs old at the time, was connected with me and we got married in October 2021. I appreciate Mrs Fatima's passion for connecting people and wish her continued success",
+  },
+  {
+    id: 4,
+    name: "Chinedu Nwaike",
+    image: chinedu,
+    text: "Married Chioma Emelugo on 23rd April 2022 in Anambra state. Contacted Mrs Fatima in June 2021 for matchmaking as I was working in Dubai and needed to settle down. She connected me with my wife in Nigeria, and we got married after 10 months of courtship. We've since relocated to Canada. Grateful to Mrs Fatima for her great work and always willing to refer people to her",
   },
 ];
 
